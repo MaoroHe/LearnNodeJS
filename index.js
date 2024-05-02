8a1c93563f113d526026c7b3007234294339ae02
MaoroHe <maorohecq1409@gmail.com>
2024-05-02 13:39:24 +0200

    staging

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
+
 const http = require('http');
+const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.write('Hello, Denis!');
     res.end();
 });
 
-server.listen(3000);
+console.log(`Server running at http://localhost:${port}/`);
+server.listen(port);
